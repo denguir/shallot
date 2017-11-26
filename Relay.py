c4e0de7e04746c2062158751e5701665ec0b1d6e
Bakaquy <p.trongquy@gmail.com>
2017-11-26 20:40:13 +0100

    new relay file

---
+++ Relay.py
@@ -0,0 +1,16 @@
+
+class Relay(object):
+    """docstring for Relay."""
+    def __init__(self, ip_addr, port):
+        superRelay, self.__init__()
+        self.ip_addr = ip_addr
+        self.port = port
+        self.key = 0
+
+    def decrypt(self, encrypted_msg):
+        ''' decrypt the message sent by previous entity'''
+        pass
+
+    def send(self, msg, ip_dest):
+        '''Send this message to the destination'''
+        pass
