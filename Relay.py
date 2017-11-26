be34c64ff68a92092a4af4f77b38f525e752be97
Bakaquy <p.trongquy@gmail.com>
2017-11-26 20:48:22 +0100

    test

--- Relay.py
+++ Relay.py
@@ -1,12 +1,10 @@
 
 class Relay(object):
     """docstring for Relay."""
-    def __init__(self, ip_addr, port):
+    def __init__(self, ip_addr):
         super(Relay, self.__init__()
         self.ip_addr = ip_addr
-        self.port = port
         self.key = 0
-	self.git_test = 0
 
     def decrypt(self, encrypted_msg):
         ''' decrypt the message sent by previous entity'''
