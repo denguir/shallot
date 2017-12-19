327213ebcb7af30819b94ab97cc4a9556a96c0ff
denguir <anass.den@gmail.com>
2017-12-19 13:32:43 +0100

    lastwithutils

--- Alice.py
+++ Alice.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
     topo = Topology()
     topo.build('config/topology.ini')
 
-    '''TEST Key init between Alice-Bob and Alice-relay1
-    Note: IP and Port are not yet implemented'''
     Alice = Sender('config/host_R1.ini')
 
     sp = Alice.shortest_path(topo,('127.16.4.2',9010))
@@ -18,10 +16,12 @@ if __name__ == '__main__':
     while Alice.init_keys_done != len(sp)-1:
         pass
 
-    message = 'Gros caca de Alice'
-    print('Message à envoyer:', message)
-
+    message = input(">>>")
     shallot = Alice.build_shallot(sp, message)
     Alice.send_shallot(sp[1][0], sp[1][1], shallot)
-    time.sleep(0.1)
+
+    time.sleep(3)
+
+    message = input(">>>")
+    shallot = Alice.build_shallot(sp, message)
     Alice.send_shallot(sp[1][0], sp[1][1], shallot)
